2b6f5b8219aaa618b4290131b2728fc73910a431
John Muchovej <5000729+jmuchovej@users.noreply.github.com>
2021-05-01 17:48:12 -0400

    📝 rm *.rst docs in place of *.md (mimics site)

--- docs/examples/docker.sh
+++ docs/examples/docker.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Check the `docker-compose.yml` file for comments on what to change
+
 docker run \
     jmuchovej/zerodns:latest \
     -n zerodns --rm \
@@ -9,4 +11,5 @@ docker run \
     -p 5053:5053/udp -p 5053:5053/tcp \
     -v "/path/to/zerodns/config:/config" \
     -e PUID=1000 -e PGID=1000 -e TZ="America/New_York" \
+    -e ACCESS_TOKEN="SomeSuperDuperSecretToken" \
     --dns 127.0.0.1 --dns 1.1.1.1 --dns 1.0.0.1
